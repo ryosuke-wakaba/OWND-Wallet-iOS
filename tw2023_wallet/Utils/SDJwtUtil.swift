3001fec0f2c96c45cf7ecd4046e875d3cb42e1e4
katsuyoshi ozaki <ozaki@datasign.jp>
2024-08-22 09:49:20 +0900

    Improved handling of nil

--- tw2023_wallet/Utils/SDJwtUtil.swift
+++ tw2023_wallet/Utils/SDJwtUtil.swift
@@ -19,8 +19,8 @@ struct Disclosure {
         self.key = key
         self.value = value
 
-        if self.disclosure != nil {
-            self.id = self.disclosure!
+        if let disclosure = self.disclosure {
+            self.id = disclosure
         }
         else {
             if let key = key, let value = value {
