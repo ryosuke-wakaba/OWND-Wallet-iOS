fd5cb0a9690c6427bd47b38f38f020ab650f3190
ozaki <ozaki@datasign.jp>
2024-10-10 12:33:28 +0900

    Add test code

---
+++ tw2023_walletTests/Utils/SerializeUtilTest.swift
@@ -0,0 +1,21 @@
+//
+//  SerializeUtilTest.swift
+//  tw2023_wallet
+//
+//  Created by katsuyoshi ozaki on 2024/10/10.
+//
+
+import XCTest
+
+@testable import tw2023_wallet
+
+final class SerializeUtilTest: XCTestCase {
+    func testToString() {
+        let data: [String: Any] = ["foo": 123, "bar": 456]
+        XCTAssertTrue((try! data.toString()) == "{\"foo\":123,\"bar\":456}")
+    }
+    func testToBase64UrlString() {
+        let data: [String: Any] = ["foo": 123, "bar": 456]
+        XCTAssertTrue((try! data.toBase64UrlString()) == "eyJmb28iOjEyMywiYmFyIjo0NTZ9")
+    }
+}
